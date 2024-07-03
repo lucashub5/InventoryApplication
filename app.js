74728ca4652b99889a83e60215f4e1d2f9c774bf
lucashub5 <lucascarovano@gmail.com>
2024-07-03 17:16:28 -0300

    update production2

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require("compression");
+const helmet = require("helmet");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
